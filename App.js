d99df0ca9648b2cdc94fb1107e2e8547d1ffeaf6
qxl0 <qxl0@yahoo.com>
2021-09-04 10:44:52 -0400

    added stack navigation: home/details

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const stack = createStackNavigator();
+
+function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Show me the Details screen"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function Details({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" 
+        onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="Home">
+        <stack.Screen name="Home" component={Home} />
+        <stack.Screen name="Details" component={Details} />
+      </stack.Navigator>
+    </NavigationContainer> 
   );
 }
 
